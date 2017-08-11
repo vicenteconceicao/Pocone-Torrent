3cbca01ab3695db69722f9443d9dd2239156be70
vicentejrlive <vicente.junior@live.com>
2017-08-11 09:20:22 -0400

    Passando intent CompartSelectFile para CompartInfo

--- app/src/main/java/br/ic/ufmt/quick/CompartInfo.java
+++ app/src/main/java/br/ic/ufmt/quick/CompartInfo.java
@@ -1,7 +1,10 @@
 package br.ic.ufmt.quick;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CompartInfo extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class CompartInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compart_info);
+
+        Intent intent = getIntent();
+
+        TextView fileName = (TextView) findViewById(R.id.fileName1);
+
+        final Uri fileUri = (Uri)intent.getParcelableExtra("path");
+
+        fileName.setText(fileUri.getPath());
     }
 }
--- app/src/main/java/br/ic/ufmt/quick/CompartSelectFile.java
+++ app/src/main/java/br/ic/ufmt/quick/CompartSelectFile.java
@@ -1,7 +1,11 @@
 package br.ic.ufmt.quick;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CompartSelectFile extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class CompartSelectFile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compart_select_file);
+
+        Button btnCompartilhar = (Button) findViewById(R.id.btn_select_compart);
+        btnCompartilhar.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                onClickChooseFile();
+            }
+        });
+
+    }
+
+    public void onClickChooseFile(){
+        Intent intent = new Intent();
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Selecionar arquivo"), 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        TextView nomeArq = (TextView) findViewById(R.id.nome_arquivo);
+
+        if(data != null) {
+            nomeArq.setText(data.getData().getPath());
+            Intent intent = new Intent(this, CompartInfo.class);
+            intent.putExtra("path", data.getData());
+            startActivity(intent);
+        }
     }
 }
